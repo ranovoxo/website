2742098aa4cbb2ec48e5dd25b7601e5a1f41f1b2
Randal Carr <randalrcarr@gmail.com>
2021-11-17 11:08:14 -0600

    config changes

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_619zb4qT3', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3us73p51t284qmdtdt1fdlf3ds', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
